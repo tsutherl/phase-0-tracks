d1287eff92b3aff46d8be4c03a4677ba11312828
Tatiana Sutherland <tsutherl@oberlin.edu>
2016-06-16 00:47:50 -0500

    Finish release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+puts "Welcome to Werewolf Inc."
+puts "What is your name?"
+name=gets.chomp
+puts "How old are you?" 
+age=gets.to_f
+puts "What year were you born?"
+year_of_birth=get.to_i
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you (yes or no)?"
+garlic_bread=gets.chomp
+puts "Would you like to enroll in the company’s health insurance (yes or no)?"
+health_insurance=get.chomp
