b131ce59348f3fa083f66886369f4c75ec3f547d
Tatiana Sutherland <tsutherl@oberlin.edu>
2016-06-22 17:10:10 -0500

    Give user an opportunity to update a key in challenge 5.3

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,6 +1,6 @@
 #tell user what they are about to do √
 #create hash √
-#Prompt the designer for information:
+#Prompt the designer for information: √
     #name: "Yuki", √ 
     #age: 26, √
     #number_of_children: 3, √
@@ -10,12 +10,24 @@
     #likes_lamps: true, √
     #note: "Likes lollipops a lot so needs washable sofa" √
 #Convert any user input to appropriate data type.
-#Print a hash back out to the screen when the designer has answered all of the questions.
-#Give the user the opportunity to update a key
-    #If the designer says "none", skip
-    #if the designer enters a key
-        #ask for new value and update the key's value. (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.)
-#Print latest version of the hash, and exit the program.
+#Print a hash back out to the screen when the designer has answered all of the questions. √
+#Give the user the opportunity to update a key √
+    #If the designer says "none", skip √
+    #if the designer enters a key √
+        #ask for new value and update the key's value. √ 
+#Print latest version of the hash, and exit the program. √
+
+def cleanup_data(client_info)
+  client_info.each do |label, value|
+  case label
+  when :age || :number_of_children
+    client_info[label] = value.to_i
+  break
+  when :has_dog || :likes_lamps
+    client_info[label] = value.
+
+
+
 
 puts "Please be ready to enter your client's information:"
 
@@ -46,6 +58,20 @@ puts "Any extra notes you'd like to add about your client?"
   client_info[:note] = gets.chomp
 
 
+puts client_info
 
+loop do
+  puts "Please list any label you'd like to update - type 'none' to skip."
+  update = gets.chomp
+
+  if update == "none"
+    puts client_info
+    exit
+  else
+    puts "Please enter new information to update the label."
+    client_info[update.to_sym] = gets.chomp
+  end
+end
+ 
 
 
