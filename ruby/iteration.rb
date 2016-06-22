c4372de09c7639be05387f4f0da1973df222c3ef
Tatiana Sutherland <tsutherl@oberlin.edu>
2016-06-21 19:08:24 -0500

    Finish challenge 5.4 @christinec01

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -21,3 +21,47 @@ to_do_list.map! { |task| task << " √"}
 
 puts to_do_list
 
+array = [1, 2, 3, 4, 5, 6]
+p array
+array.delete_if {|number| number<5 }
+p array
+
+hash = {
+  "First_name" => "Bob",
+  "Lastname" => "Roberts",
+  "City" =>"Flower Town",
+  "State" =>"FL",
+}
+
+hash.delete_if { |label, answer| label == "Lastname"}
+
+p hash
+
+array.keep_if {|number| number.even?}
+p array
+
+hash.keep_if {|label,answer| label=="City"}
+p hash
+
+array = [1, 2, 3, 4, 5, 6]
+array.reject! { |number| number>3}
+
+p array
+
+hash = {
+  "First_name" => "Bob",
+  "Lastname" => "Roberts",
+  "City" =>"Flower Town",
+  "State" =>"FL",
+}
+
+hash.reject! {|label, answer| label == "First_name"}
+
+p hash
+
+array = [1, 2, 3, 4, 5, 6]
+array.drop_while { |i| i < 3}
+
+p array
+
+#The hash equivalent for Array#drop_while that we would chose to use on a block of code would be the Hash#.reject!
