68f2f39d55299246d122e16509f55b40a478763b
Tatiana Sutherland <tsutherl@oberlin.edu>
2016-07-03 21:58:33 -0500

    Finish release 0 in challenge 6.3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,22 @@
+class Santa
+
+  def initialize
+    puts "Initializing Santa instance ..."
+  end
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie_type)
+    puts "That was a good #{cookie_type}!"
+  end
+
+end
 
 
+santa1 = Santa.new
 
+santa1.speak
 
+santa1.eat_milk_and_cookies("chocolate chip cookie")
