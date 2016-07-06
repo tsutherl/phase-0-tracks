529aada93c8b1105207a9e4b62da48a198a29496
Tatiana Sutherland <tsutherl@oberlin.edu>
2016-07-05 21:22:06 -0500

    Design class in challenge 6.4

---
+++ ruby/solo.rb
@@ -0,0 +1,17 @@
+#CLASS -------------------
+#Candy
+
+#ATTRIBUTES -----------
+#Type: varies(string)
+#Color: varies(string)
+#Size: varies(string)
+#Number_of_pieces: varies(float)
+#Sweet: varies(boolean)  
+
+#METHODS -----------------
+#Eaten
+#Dropped
+#Hidden(location) 
+#Add_pieces(number_of_new_pieces) 
+#Melted
+#Went_through_laundry(ruined_clothing)
